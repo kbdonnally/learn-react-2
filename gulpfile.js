6dfe7d03c205a42351989443bfa88bf9d1584536
Katherine Donnally <kbd3ax@virginia.edu>
2019-12-02 13:37:14 -0500

    Update gulpfile and add new dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -15,3 +15,35 @@ const js = () => {
 };
 
 gulp.task('js', js);
+
+// ----------------------------------------------------
+
+const babelCompile = (done) => {
+	gulp.src('babel/*.js')
+		.pipe(babel({
+			presets: ["@babel/preset-react"]
+		}))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('js'))
+	done();
+};
+
+const babelClean = (done) => {
+	gulp.src('js/*.js', {allowEmpty: true})
+		.pipe(clean());
+	done();
+};
+
+gulp.task('babel:compile', babelCompile);
+
+gulp.task('babel:watch', () => {
+	return gulp.watch('babel/*.js', babelCompile)
+});
+
+gulp.task('babel:clean', babelClean);
+
+// gulp babel
+gulp.task('babel', gulp.series('babel:clean', 'babel:compile', 'babel:watch'));
+
+// Explanation: put all Babel files into 'assets/js/babel/*.js'
+// Then it'll concatenate everything and minify
