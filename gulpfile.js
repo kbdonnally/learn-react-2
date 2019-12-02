01346834667374a01087bc01e39547f23550852f
Katherine Donnally <kbd3ax@virginia.edu>
2019-12-02 12:41:07 -0500

    Update gulpfile to concatenate & minify js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,17 @@
 // gulpfile.js 
-const gulp = require('gulp');
-const responsive = require('gulp-responsive');
+const gulp   = require('gulp');
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
 
-const imageResize = () => {
-	return gulp.src('assets/img/screenshots/takeback/*.png')
-		.pipe(responsive({
-			'*.png': [{
-				width: 400,
-				rename: {
-					suffix: '-400px'
-				}
-			},
-			{
-				width: 800,
-				rename: {
-					suffix: '-800px'
-				},
-				withoutEnlargement: true
-			}]
+// ---------------------------------------------------
+
+const js = () => {
+	return gulp.src('./assets/js/partials/*.js')
+		.pipe(concat('main.js'))
+		.pipe(minify({
+			ext: { min: '.min.js' }
 		}))
-		.pipe(gulp.dest('assets/img/screenshots/takeback'));
+		.pipe(gulp.dest('./assets/js'));
 };
 
-gulp.task('images', imageResize);
+gulp.task('js', js);
