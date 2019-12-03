38329d6f853b2856234f3322e2108807bef20ebd
Katherine Donnally <kbd3ax@virginia.edu>
2019-12-03 17:49:24 -0500

    Add missing semicolon

--- assets/js/partials/hello-world-2.js
+++ assets/js/partials/hello-world-2.js
@@ -4,7 +4,7 @@
 if (document.getElementById('hello-world-2')) {
 	class MyComponentClass extends React.Component {
 		render() {
-			return <h1>Hello, world!</h1>
+			return <h1>Hello, world!</h1>;
 		}
 	};
 
