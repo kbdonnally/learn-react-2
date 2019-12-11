2d422ee75cfc4e1d495e7e422145f7a287684e2e
Katherine Donnally <kbd3ax@virginia.edu>
2019-12-11 17:50:38 -0500

    Add explainer to self about how children pass state to parents

--- assets/js/dist/Child.js
+++ assets/js/dist/Child.js
@@ -8,6 +8,8 @@ export class Child extends React.Component {
     // NB: 'value' is an attribute we gave to each option
     const name = e.target.value;
     this.props.onChange(name); // run this code when clicked
+
+    console.log(this.props);
   }
 
   render() {
@@ -25,3 +27,29 @@ export class Child extends React.Component {
   }
 
 } // end <Child />
+
+/*
+
+What happens:
+
+1. <select> gets clicked on, which triggers onChange (reserved phrase for forms)
+2. In Child: this.handleChange is called
+	- Identifies e.target.value as the name;
+   	- Feeds the name as this.props.onChange(name)
+3. In the Parent, the call to onChange triggers this.changeName(name) with the same 'name' variable passed through;
+   updates the name
+   	- This is how the actual name gets changed in state for Parent
+   	- The changed state is now accessible in Child as a prop
+4. State is updated in Parent, used to render `name={this.state.name}` in Child
+5. This means Child can now use {this.props.name}, because now it has a prop named 'name'
+	- This is arbitrary; it could be named 'pudding' or something; it's for consistency
+
+- Clicked on thing
+- Child function called that uses the raw data from the interaction
+- Child function calls Parent function
+- Parent function changes state
+- Parent function passes the state values back to Child instances as a prop
+
+- Child processes data info and sends it to parent, parent does the actual updating of its state
+
+*/
