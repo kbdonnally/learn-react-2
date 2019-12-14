a24b05603f50f846804f9e7d166ff05fc598fc73
Katherine Donnally <kbd3ax@virginia.edu>
2019-12-14 12:54:00 -0500

    Made GuineaPigs a stateless functional component

--- assets/js/dist/GuineaPigs.js
+++ assets/js/dist/GuineaPigs.js
@@ -1,13 +1,6 @@
 /* Goes with guinea-pigs.html */
-
-/* Goes with guinea-pigs.html */
-export class GuineaPigs extends React.Component {
-  render() {
-    let src = this.props.src; // gets passed in render of container, = current GP
-
-    return React.createElement("div", null, React.createElement("h1", null, "Cute Guinea Pigs"), React.createElement("img", {
-      src: src
-    }));
-  }
-
-}
+export const GuineaPigs = props => {
+  return React.createElement("div", null, React.createElement("h1", null, "Guinea Pig Cutie Pies"), React.createElement("img", {
+    src: props.src
+  }));
+};
--- assets/js/partials/GuineaPigs.js
+++ assets/js/partials/GuineaPigs.js
@@ -1,15 +1,10 @@
 /* Goes with guinea-pigs.html */
 
-/* Goes with guinea-pigs.html */
-
-export class GuineaPigs extends React.Component {
-	render() {
-		let src = this.props.src; // gets passed in render of container, = current GP
-		return (
-			<div>
-				<h1>Cute Guinea Pigs</h1>
-				<img src={src} />
-			</div>
-		);
-	}
+export const GuineaPigs = (props) => {
+	return (
+		<div>
+			<h1>Guinea Pig Cutie Pies</h1>
+			<img src={props.src} />
+		</div>
+	);
 }
